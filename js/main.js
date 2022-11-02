7e110aea7f56dbd38d0c705d561d8633cbba37d7
minh15092003 <100700718+TinyTots-ePj@users.noreply.github.com>
2022-11-02 22:55:57 +0700

    add .

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// khai bao cac bien 
+
+var fullName = 'Minh Oc';
+var age = 19
+var a = 1;
+var b = 2;
+
+ 
+// Chay cac bien 
+ 
+    console.log(fullName)
+    console.log(a + b)
+
+    if(a < b){
+        console.log('Đúng')
+    }
+
+    
+// Confirm 
+      
+   // confirm('xac nhan ban du tuoi...!')
+
+// SetInterval
+
+ setInterval(function() {
+    console.log('Tôi là Minh Ốc..' + Math.random())
+ },1000)
+
+
+{
+    var course = 'Minh ốc'
+}
+
+    console.log(course)
